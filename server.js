44411cc24d127d5fb2c97d849ff0bf548ea15394
Abhay Kumar <abhay0480sharma@gmail.com>
2026-04-07 17:34:57 +0530

    routig na dpaths intro

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ import http from 'node:http'
 
 const PORT = 8000
 
+const __dirname = import.meta
+console.log("__dirname", __dirname);
+
+const pathname = `${__dirname}/public/index.html`
+
+console.log("pathname", pathname)
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<html><head></head><body><h1>Hello World</h1></body></html>')
+    res.setHeader('Content-Type', 'text/html')
+    res.end("<html><head></head><body><h1>Hello World</h1></body></html>")
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
