b5d9bb3526d78d3ce83532f9af6e9ba01f2a7a84
Abhay Kumar <abhay0480sharma@gmail.com>
2026-04-07 18:39:03 +0530

    absolute nd relative path

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 import http from 'node:http'
+import path from 'node:path'
 
 const PORT = 8000
-
-const __dirname = import.meta
-console.log("__dirname", __dirname);
-
-const pathname = `${__dirname}/public/index.html`
-
-console.log("pathname", pathname)
+const __dirname = import.meta.dirname
 
 const server = http.createServer((req, res) => {
+    const pathToResource = path.join(__dirname, 'public', 'index.html')
+    console.log("pathname", pathToResource);
+
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end("<html><head></head><body><h1>Hello World</h1></body></html>")
+    res.end('<html><h1>Hello World</h1></html>')
 })
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
